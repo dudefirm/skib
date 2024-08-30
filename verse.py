ce86066eb81bdc167a75d3edb3798c76c63bdf45
dudefirm <meepinmeep07@gmail.com>
2024-08-30 15:06:51 -0500

    Add files via upload

---
+++ verse.py
@@ -0,0 +1,21 @@
+def verse_1():
+    print("cause I see what you did to me")
+    print("and I can't let nobody tell me who I am.")
+
+def verse_2():
+    print("Have we fallen in lovе?")
+    print("Have we fallen in love? Again.")
+    print("Riovaz - I wonder, rate 1 thru 10")
+
+def printing():
+    x = input()
+    rating = input("Thank you!")
+     
+
+
+
+
+if __name__ == "__main__":
+    verse_1()
+    verse_2()
+    printing()

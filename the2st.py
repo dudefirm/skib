deff6c2d1464ff6fd22709ccf92cb974d35dd44e
dudefirm <meepinmeep07@gmail.com>
2024-09-16 15:29:17 -0500

    Add files via upload

---
+++ the2st.py
@@ -0,0 +1,38 @@
+from turtle import Turtle, exitonclick
+
+
+def baton():
+    bob = Turtle()
+    bob.color("blue")
+    bob.forward(180)
+    bob.left(90)
+    bob.forward(75)
+    bob.left(90)
+    bob.forward(100)
+    bob.right(90)
+    bob.forward(125)
+    bob.left(90)
+    bob.forward(100)
+    bob.left(90)
+    bob.forward(125)
+    bob.right(90)
+    bob.forward(125)
+    bob.forward(180)
+    bob.left(90)
+    bob.forward(75)
+    bob.left(90)
+    bob.forward(325)
+    bob.penup()
+    bob.left(90)
+    bob.forward(500000)
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    baton()
+    exitonclick()
